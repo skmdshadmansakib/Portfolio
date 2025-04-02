a1da1f53c05c2c9e76bced9a2acf1a3012276caa
skmdshadmansakib <skmdshadmansakib@gmail.com>
2025-04-02 07:37:40 +0600

    asda

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb'],
-  base: '/Portfolio/', // Make sure this is set correctly for GitHub Pages
-});
+  assetsInclude: ['**/*.glb']
+})
