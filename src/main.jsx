272b6c7a614a6d0ed1b1f028658555554d34f856
skmdshadmansakib <skmdshadmansakib@gmail.com>
2025-04-02 07:23:38 +0600

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { HashRouter as Router } from "react-router-dom";  // ✅ Use HashRouter
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router> 
-    <App />
-  </Router>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
