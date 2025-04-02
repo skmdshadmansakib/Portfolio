772e323e6beaffe966b436c68b262cb1f00f701e
skmdshadmansakib <skmdshadmansakib@gmail.com>
2025-04-02 07:13:41 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { HashRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
-    <App />
-  </Router>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
